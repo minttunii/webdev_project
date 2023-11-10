61ba7aafb29c4f1e4ac06e300c1c5008c38d863a
Lotta Hyvärinen <lotta.hyvarinen@tuni.fi>
2023-11-10 13:59:54 +0200

    Index.js modified to use database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const { handleRequest } = require('./routes');
+const { connectDB } = require('./models/db');
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(handleRequest);
+
+const dburl = process.env.DBURL;
+connectDB();
 
 server.on('error', err => {
   console.error(err);
